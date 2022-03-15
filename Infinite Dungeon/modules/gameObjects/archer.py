d213b47033d919ab5aca14cd5f6dda421e5adc3b
nicholasqnguyen8 <nicholasqnguyen8@gmail.com>
2022-03-15 18:28:00 -0400

    Put archer HP bak to normal from testing

--- Infinite Dungeon/modules/gameObjects/archer.py
+++ Infinite Dungeon/modules/gameObjects/archer.py
@@ -16,8 +16,8 @@ from ..managers.frameManager import FrameManager
 from ..managers.itemManager import BasicItemManager
 
 
-# ARCHER_HP = 50
-ARCHER_HP = 5
+ARCHER_HP = 50
+# ARCHER_HP = 5
 ARCHER_V_SPEED = 150
 # ARCHER_V_SPEED = 550
 
